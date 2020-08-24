a433ae6fe05ce9d503cd8e0270bb1b9e9c2b8440
Manu Hdez <manu.hdez.dev@gmail.com>
2020-08-24 22:54:19 +0100

    improved event bus loggin

--- event-bus/src/app.ts
+++ event-bus/src/app.ts
@@ -21,20 +21,26 @@ app.post('/events', (req: Request, res: Response) => {
 
   if (type) {
     events.storeEvent({ type, payload });
-  }
 
-  axios
-    .post(process.env.POSTS_SERVICE_URL as string, { type, payload })
-    .catch((err) => console.log('error passing event to posts service'));
-  axios
-    .post(process.env.COMMENTS_SERVICE_URL as string, { type, payload })
-    .catch((err) => console.log('error passing event to comments service'));
-  axios
-    .post(process.env.QUERY_SERVICE_URL as string, { type, payload })
-    .catch((err) => console.log('error passing event to query service'));
-  axios
-    .post(process.env.MODERATION_SERVICE_URL as string, { type, payload })
-    .catch((err) => console.log('error passing event to moderation service'));
+    console.log(`Event ${type} received`);
+
+    axios
+      .post(process.env.POSTS_SERVICE_URL as string, { type, payload })
+      .then(() => console.log(`Event ${type} redirected to posts service`))
+      .catch((err) => console.log('error passing event to posts service'));
+    axios
+      .post(process.env.COMMENTS_SERVICE_URL as string, { type, payload })
+      .then(() => console.log(`Event ${type} redirected to comments service`))
+      .catch((err) => console.log('error passing event to comments service'));
+    axios
+      .post(process.env.QUERY_SERVICE_URL as string, { type, payload })
+      .then(() => console.log(`Event ${type} redirected to query service`))
+      .catch((err) => console.log('error passing event to query service'));
+    axios
+      .post(process.env.MODERATION_SERVICE_URL as string, { type, payload })
+      .then(() => console.log(`Event ${type} redirected to moderation service`))
+      .catch((err) => console.log('error passing event to moderation service'));
+  }
 
   res.status(200).json({ success: true });
 });
